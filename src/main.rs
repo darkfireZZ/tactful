c7bf2682be9cc77d07eef5a47ee4f74d2094ff34
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2023-08-22 17:57:13 +0200

    Create rough outline of structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use rust_iso3166::CountryCode;
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Contact {
+    name: Name,
+    birthday: Option<Date>,
+    phone_numbers: Vec<PhoneNumber>,
+    email_addresses: Vec<EmailAddress>,
+    address: Option<Address>,
+}
+
+struct Name {
+    first: String,
+    last: String,
+}
+
+struct Date {
+    year: Option<u16>,
+    month: Option<u8>,
+    day: Option<u8>,
+}
+
+struct PhoneNumber {
+    number: String,
+    ty: PhoneNumberType,
+}
+
+enum PhoneNumberType {
+    Mobile,
+    Home,
+    Work
+}
+
+struct EmailAddress {
+    address: String,
+    ty: EmailAddressType,
+}
+
+enum EmailAddressType {
+    Personal,
+    Work,
+}
+
+struct Address {
+    street: String,
+    number: String,
+    locality: String,
+    postal_code: String,
+    country: CountryCode,
 }
