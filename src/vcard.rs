25348a55b77c4764c6cfbeb852142644ca7b7b87
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2023-08-25 21:48:50 +0200

    Finish vCard conversion

--- src/vcard.rs
+++ src/vcard.rs
@@ -22,6 +22,10 @@ where
     Ok(())
 }
 
+// TODO this function is very ugly, make it cleaner. The problem is mostly due to the `ical_vcard`
+// crate not being very ergonomic. All its functions return results which adds a lot of clutter and
+// thus makes this function very unreadable. Solution: Add panicking variants of the functions to
+// `ical_vcard`.
 fn contact_to_contentlines(contact: &Contact) -> anyhow::Result<Vec<Contentline<'static>>> {
     let mut contentlines = vec![
         Contentline {
@@ -75,11 +79,35 @@ fn contact_to_contentlines(contact: &Contact) -> anyhow::Result<Vec<Contentline<
                 .expect("valid parameter"),
             ],
             value: Value::new(format!("tel:{formatted_number}")).expect("valid value"),
-        })
+        });
     }
 
-    // TODO implement email addresses
-    // TODO implement physical address
+    for email_address in &contact.email_addresses {
+        contentlines.push(Contentline {
+            group: None,
+            name: Identifier::new("EMAIL").expect("valid identifier"),
+            params: Vec::new(),
+            value: Value::new(email_address.to_owned())
+                .context("Failed to write email address to contentline")?,
+        });
+    }
+
+    if let Some(address) = &contact.address {
+        contentlines.push(Contentline {
+            group: None,
+            name: Identifier::new("ADR").expect("valid identifier"),
+            params: Vec::new(),
+            value: Value::new(format!(
+                ";;{street} {number};{locality};;{postal_code};{country}",
+                street = address.street,
+                number = address.number,
+                locality = address.locality,
+                postal_code = address.postal_code,
+                country = address.country.name
+            ))
+            .context("Failed to write address to contentline")?,
+        });
+    }
 
     if let Some(birthday) = &contact.birthday {
         contentlines.push(Contentline {
@@ -92,7 +120,7 @@ fn contact_to_contentlines(contact: &Contact) -> anyhow::Result<Vec<Contentline<
                     .context("Failed to write birthday to contentline")?,
             )
             .expect("valid value"),
-        })
+        });
     }
 
     contentlines.push(Contentline {
